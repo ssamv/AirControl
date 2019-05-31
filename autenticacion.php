80164cfc2da2fcfaf5624abc8d17cfb053c6e276
ssamv <40603323+ssamv@users.noreply.github.com>
2019-05-31 10:33:38 -0400

    ahora lo arreglo

--- autenticacion.php
+++ autenticacion.php
@@ -15,21 +15,3 @@ $existe= mysqli_num_rows($resultado);
     echo "<script language='javascript'>window.location='index.php'</script>";
   }
 ?>
-
-<?php
-session_start();
-require("database.php");
-$user= $_POST['usuario'];
-$pswd= $_POST['password'];
-$consulta_inicio= "SELECT * FROM Admin WHERE nom_usuario ='$user' AND contraseña = '$pswd'";
-$resultado= mysqli_query($link,$consulta_inicio) or die('Consulta fallida: ' . mysqli_error());
-$existe= mysqli_num_rows($resultado);
-  if($existe == 1){
-    $_SESSION['user_inside'] = $user;
-    echo "<script language='javascript'>window.location='HomeAdmin.php'</script>";
-   }
-  else {
-    echo "<script language='javascript'>alert('Usuario y/o Contraseña erroneos, intente nuevamente');</script>";   
-    echo "<script language='javascript'>window.location='index.php'</script>";
-  }
-?>
